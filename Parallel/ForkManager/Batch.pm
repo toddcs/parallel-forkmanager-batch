b5caa4c631d952105307e0b8bf6fa0efabb3e2c8
Todd Shandelman <tshandelman@hostgator.com>
2014-12-24 14:35:23 -0600

    Parallel::ForkManager::Batch , per R. Stone.

---
+++ Parallel/ForkManager/Batch.pm
@@ -0,0 +1,40 @@
+package Batch;
+
+use Parallel::ForkManager;
+
+sub new {
+    my ( $class, $max_procs, $batch_size, $batch_records, $doit ) = @_;
+
+    my $self = {};
+
+    $self->{max_procs}     = $max_procs;
+    $self->{batch_size}    = $batch_size;
+    $self->{batch_records} = $batch_records;
+    $self->{doit}          = $doit;
+
+    return bless $self;
+}
+
+sub run {
+    $self = shift;
+
+    my $pfm = new Parallel::ForkManager( $self->{max_procs} );
+
+    $num_records = @{ $self->{batch_records} };
+    $batch_size  = $self->{batch_size};
+
+    for ( my $i = 0 ; $i < $num_records ; $i += $batch_size ) {
+        my $pid = $pfm->start and next;
+
+        for ( my $j = $i ; $j < $i + $batch_size ; ++$j ) {
+            last if $j >= scalar @{ $self->{batch_records} };
+            $self->{doit}->( $self->{batch_records}[$j] );
+        }
+
+        $pfm->finish;
+    }
+
+    $pfm->wait_all_children;
+}
+
+1;
---
+++ test.pl
@@ -0,0 +1,24 @@
+#!/usr/bin/env perl
+
+use strict;
+use warnings;
+
+use Parallel::ForkManager::Batch;
+
+my $MAX_PROCESSES = 3;
+my $BATCH_SIZE    = 8;
+
+srand 9876543210;
+
+my @batch;
+for ( 1 .. 50 ) { push @batch, int rand 1_000; }
+
+sub do_one {
+    my $record = shift;
+    printf "child: record = [[%s]]\n", $record;
+}
+
+my $batch = Batch->new( $MAX_PROCESSES, $BATCH_SIZE, \@batch, \&do_one );
+
+$batch->run;
+
