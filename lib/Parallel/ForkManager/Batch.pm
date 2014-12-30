2b8b146af19de90cb8a6ecdc0d27a52daa43e7b1
Todd Shandelman <tshandelman@hostgator.com>
2014-12-30 11:04:36 -0600

    ** WORK IN PROGRESS **

--- lib/Parallel/ForkManager/Batch.pm
+++ lib/Parallel/ForkManager/Batch.pm
@@ -5,6 +5,55 @@ use warnings;
 
 use Parallel::ForkManager;
 
+# VERSION: 0.1.0
+
+# ABSTRACT: Calls Parallel::ForkManager on batches of tasks
+
+#=head2 FIXME Add POD here
+#
+#Also, verify that your cpan "pause" account is ready. You have requested it:
+#
+#  #  Request PAUSE account
+#  #  
+#  #  A PAUSE account is only required to distribute and manage Perl module
+#  #  distributions on CPAN. You do not need a PAUSE account to submit bug reports to
+#  #  RT or participate in many Perl community sites — please register a Bitcard
+#  #  account instead.
+#  #  
+#  #  Sending mail to: modules@perl.org todd.shandelman@gmail.com
+#  #  From: upload@pause.perl.org
+#  #  Subject: PAUSE ID request (TODDCS; Todd Shandelman)
+#  #  
+#  #  Request to register new user
+#  #  
+#  #  fullname: Todd Shandelman
+#  #    userid: TODDCS
+#  #      mail: CENSORED
+#  #  homepage: 
+#  #       why:
+#  #  
+#  #      Isn't it about time I give back to the developer community? I would like to
+#  #      start contributing, finally, after being a Perl developer for a very long
+#  #      time. (I'd rather not tell you how long :-)
+#  #  
+#  #  
+#  #  The following links are only valid for PAUSE maintainers:
+#  #  
+#  #  Registration form with editing capabilities:
+#  #    https://pause.perl.org/pause/authenquery?ACTION=add_user&
+#  #      USERID=c1110000_8f1dc0184adc147a&SUBMIT_pause99_add_user_sub=1
+#  #  Immediate (one click) registration:
+#  #    https://pause.perl.org/pause/authenquery?ACTION=add_user&
+#  #      USERID=c1110000_8f1dc0184adc147a&SUBMIT_pause99_add_user_Definitely=1
+#
+#Also dist.ini ;
+#
+#Publish the new module on cpan.
+#
+#Also refactor existing code in gator-bill-crons to use the new module.
+#
+#=cut
+
 sub new {
     my ( $class, $max_procs, $batch_size, $batch_records, $doit ) = @_;
 
@@ -41,5 +90,115 @@ sub run {
 
     return;
 }
+
+=head1 NAME
+
+    Parallel::ForkManager::Batch
+
+=head1 SYNOPSIS
+
+    my $MAX_PROCESSES = ... ;          # Passed thru to Parallel::ForkManager
+    my $BATCH_SIZE    = ... ;          # Max number of tasks per forked process
+
+    my $tasks_to_batch = [
+                    # ...
+                ];
+
+    sub do_one_task {
+        # ... process one batch task
+    }
+
+    my $batch = Parallel::ForkManager::Batch->new( $MAX_PROCESSES, $BATCH_SIZE, $tasks_to_batch, \&do_one_task );
+
+    $batch->run;
+
+=head1 DESCRIPTION
+
+Parallel::ForkManager::Batch runs batches of processing using
+Parallel::ForkManager as its forking engine. But rather than forking a
+dedicated process for each individual task, as Parallel::ForkManager
+would normally do, this module arranges for the forking manager to
+create only one process for each batch of tasks.
+
+Each batch consists of a predefined number of tasks (except that
+the final batch might be smaller).
+
+The current module provides a simple interface for such batching of
+tasks within processes.
+
+=head1 METHODS
+
+=head2 new()
+
+    $batch = Parallel::ForkManager::Batch->new( $max_processes, $batch_size, $tasks_to_batch, \&do_one_task );
+
+Calling C<new()> creates and returns a runnable batch object, according to the specs provided by its
+arguments. These are:
+
+=over 4
+
+C<$max_processes>
+
+=over 4
+
+The maximum number of processes we want to exist at any given time. This argument is passed thru to
+Parallel::ForkManager , who manages this constraint.
+
+=back
+
+C<$batch_size>
+
+=over 4
+
+The maximum number of tasks that will be batched together for processing by one process created by
+Parallel::ForkManager .
+
+=back
+
+C<$tasks_to_batch>
+
+=over 4
+
+A reference to an array containing tasks to be batched.
+
+=back
+
+C<\&do_one_task>
+
+=over 4
+
+A code reference, pointing to a subroutine that will be called repeatedly
+to process batches of tasks.  For each batch, this subroutine will
+be called $BATCH_SIZE times (except for the final batch, which may be
+smaller). This module arranges that each time the subroutine is called,
+it will receive exactly one argument, a single task from the batch list.
+
+=back
+
+=back
+
+=cut
+
+=head2 run()
+
+    $batch->run()
+
+This method performs all the "heavy lifting" of combining individual tasks
+into batches and passing the batches to Parallel::ForkManager . This includes
+C<start()>ing each batch in the parallel fork manager, and waiting for all batches to complete.
+This method takes no arguments and returns nothing.
+
+=head1 COPYRIGHT
+
+This module is free software; you can redistribute it and/or modify it
+under the same terms as Perl itself.
+
+=head1 AUTHOR
+
+Todd Shandelman (C<< todd.shandelman@gmail.com >>)
+
+Robert Stone (C<< drzigman@cpan.org >>)
+
+=cut
 
 1;
